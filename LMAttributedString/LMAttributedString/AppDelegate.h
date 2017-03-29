460cf949bcb25011133967e526f32a3f0c069b1d
KADFWJ <930135305@qq.com>
2017-03-29 10:19:05 +0800

    还是测试

---
+++ LMAttributedString/LMAttributedString/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMAttributedString
+//
+//  Created by KADFWJ on 2017/3/29.
+//  Copyright © 2017年 Lemon Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LMAttributedString/LMAttributedString/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LMAttributedString
+//
+//  Created by KADFWJ on 2017/3/29.
+//  Copyright © 2017年 Lemon Fan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
